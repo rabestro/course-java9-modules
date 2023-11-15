54cf7f4a1a2c9c1ce1b08703323b169e0165cd70
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-11-15 23:43:36 +0200

    feat: Implement createBankCard method in BankCloudImpl and add Spock tests
    - Implement the createBankCard method in the BankCloudImpl class to create credit and debit bank cards for valid users.
    - Add a Spock test class BankCloudImplSpec with the following test cases:
      1. Test creating a credit bank card for a valid user.
      2. Test creating a debit bank card for a valid user.
      3. Test creating a bank card with a null user.
      4. Test creating a bank card with a null bank card type.
    - Use the LuhnCardNumberSupplier class to generate valid card numbers for the created bank cards.
    - Add appropriate error handling for null user and null bank card type inputs.

--- jmp-cloud-bank-impl/src/main/java/com/epam/engx/jmp/cloud/bank/impl/BankCloudImpl.java
+++ jmp-cloud-bank-impl/src/main/java/com/epam/engx/jmp/cloud/bank/impl/BankCloudImpl.java
@@ -3,12 +3,25 @@ package com.epam.engx.jmp.cloud.bank.impl;
 import com.epam.engx.jmp.bankapi.Bank;
 import com.epam.engx.jmp.dto.BankCard;
 import com.epam.engx.jmp.dto.BankCardType;
+import com.epam.engx.jmp.dto.CreditBankCard;
+import com.epam.engx.jmp.dto.DebitBankCard;
 import com.epam.engx.jmp.dto.User;
+
+import java.util.Objects;
 
 public final class BankCloudImpl implements Bank {
 
 	@Override
 	public BankCard createBankCard(User user, BankCardType type) {
-		return null;
+		Objects.requireNonNull(user, "User cannot be null");
+		Objects.requireNonNull(type, "Bank card type cannot be null");
+
+		var cardNumberSupplier = new LuhnCardNumberSupplier();
+		var cardNumber = cardNumberSupplier.get();
+
+		return switch (type) {
+			case CREDIT -> new CreditBankCard(cardNumber, user);
+			case DEBIT -> new DebitBankCard(cardNumber, user);
+		};
 	}
 }
