e22b10e7cae2ba16a7abc832a2fde19b0a5afb97
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-11-19 17:38:35 +0200

    Add new API endpoints and refactor the existing ones
    Added new endpoints and refactored the existing ones in the BankController and ServiceController files for better functionality and testing. The new endpoints provide the ability to retrieve a bank card by its number and to subscribe to a service using the bank card number. Moreover, the '/createCard' endpoint in BankController has been refactored to '/card' to follow RESTful convention. In addition, test dependencies have been added and new tests have been written to verify the updated functionality.

--- jmp-application/src/main/java/com/epam/jmp/application/controller/BankController.java
+++ jmp-application/src/main/java/com/epam/jmp/application/controller/BankController.java
@@ -1,15 +1,18 @@
 package com.epam.jmp.application.controller;
 
-import com.epam.jmp.api.Bank;
+import java.util.ServiceLoader;
+
+import com.epam.jmp.dto.BankCard;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ServiceLoader;
+import com.epam.jmp.api.Bank;
 
 @RestController
 @RequestMapping("/api")
@@ -17,9 +20,16 @@ public class BankController {
 	private final Bank bank = ServiceLoader.load(Bank.class).findFirst().orElseThrow();
 
 	@PostMapping(value = "/card")
-	@ResponseBody
 	public ResponseEntity<?> createBankCard(@RequestBody BankCardRequest request) {
 		var card = bank.createBankCard(request.user(), request.type());
 		return ResponseEntity.ok(card.number());
+	}
+
+	@GetMapping(value = "/card/{cardNumber}")
+	public ResponseEntity<?> getBankCardByNumber(@PathVariable String cardNumber) {
+		return bank.findBankCardByNumber(cardNumber)
+			.map(BankCard::user)
+			.map(ResponseEntity::ok)
+			.orElse(ResponseEntity.notFound().build());
 	}
 }
